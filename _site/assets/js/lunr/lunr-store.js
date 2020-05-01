ca4cbef742b159eaf7245a0a3c77403d98b30336
Jonathan Li <jonathan5625@gmail.com>
2020-05-01 09:48:33 -0700

    added jekyll files

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Welcome to Minimal Mistakes Jekyll",
+        "excerpt":"Welcome to the first blog posts of Minimal Mistakes Jekyll. You may edit the title, date, categories, and tags of this blog post to be your own OR delete this post and create a new one. Things to take note when creating a blog posts Categories and tags are optionals...","categories": ["update"],
+        "tags": ["jekyll"],
+        "url": "http://localhost:4000/update/welcome-to-minimal-mistakes-jekyll/",
+        "teaser": null
+      },{
+        "title": "Math Support",
+        "excerpt":"You can enable math support through use_math: true front matter which enable the MathJax.js rendering. After enable math support, use $$ ... $$ displaying math equation. For inline mode type your equation in line with other text For display mode: type your equation such that there is a blank line...","categories": [],
+        "tags": ["MathJax"],
+        "url": "http://localhost:4000/math-support/",
+        "teaser": null
+      },{
+        "title": "Sample Post",
+        "excerpt":"Lorem Ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet sem efficitur, accumsan erat at, molestie dui. Nunc id libero dolor. Duis sed enim interdum, ultrices dolor vitae, varius magna. Nullam tincidunt sit amet velit vitae gravida. Duis porttitor dignissim nulla, eget blandit purus semper vel. Maecenas...","categories": ["sample"],
+        "tags": [],
+        "url": "http://localhost:4000/sample/sample-post/",
+        "teaser": null
+      }]
