56f2b41e9e335978554be9d683d20bd66ecd2e2d
AldricSangalang <55821847+AldricSangalang@users.noreply.github.com>
2020-05-26 00:26:00 -0700

    Fixed "win" added "tie", added reset, cleaned up

--- software/microcontroller/tictactoe.ino
+++ software/microcontroller/tictactoe.ino
@@ -1,16 +1,16 @@
 #include <msp430.h>
 #include "ws2812.h"
 
-#include<stdint.h>
+#include <stdint.h>
 
 #define soundPin P1_4
 
 int sensorValue = 0;
 int eventstatus = 0;
 
-int current = 9;
-int grid[9] = {2, 2, 2, 2, 2, 2, 2, 2, 2}; // 0 means O, 1 means X, 2 means nothing
-int player = 0; // 0 means O, 1 means X
+int grid[9] = {2, 2, 2, 
+               2, 2, 2, 
+               2, 2, 2}; // 0 means O, 1 means X, 2 means nothing
 
 void toggleLED()
 {
@@ -30,71 +30,52 @@ void toggleLED()
 void setup(void) 
 {
     Serial.begin(9600);
-//    WDTCTL = WDTPW + WDTHOLD;  // Stop WDT
-//    if (CALBC1_16MHZ==0xFF)    // If calibration constant erased
-//    {
-//        while(1);              // do not load, trap CPU!!
-//    }
-//
-//    // configure clock to 16 MHz
-//    BCSCTL1 = CALBC1_16MHZ;    // DCO = 16 MHz
-//    DCOCTL = CALDCO_16MHZ;
-//
-//
-//    P1DIR |= BIT0;              // P1.0 (Red) -> Output
-//    toggleLED();
-//
-//    // initialize LED strip
-    initStrip();  // ***** HAVE YOU SET YOUR NUM_LEDS DEFINE IN WS2812.H? ******
-
+    initStrip();  
     pinMode(soundPin, INPUT);
-    
-//    drawCross(0, COLOR_GREEN);
-//    showStrip();
-//    delay(1000);
-//    clearStrip();
-//    drawCircle(0, COLOR_BLUE);
-//    showStrip();
-//    delay(1000);
-//    clearStrip();
-//    drawCircle(0, COLOR_OFF);
-//    showStrip();
-//      fillStrip(COLOR_OFF);
-//      clearStrip();
-    
 }
   
 void loop () 
 {
-
-    if (Serial.available() > 0) {
-        // Read the most recent byte
-        int intRead = Serial.read() - 48;
-
-        // swap 2 and 0 to match how LEDs are wired
-        if (intRead == 0) {
-          intRead = 2;
-        }
-        else if (intRead == 2) {
-          intRead = 0;
-        }
-
-//        Serial.println("Hello World");
-        // ECHO the value that was read
-        // Serial.write(intRead);
-
-        // limit intRead < 6 because we only have 6 LEDs
-        if (current != intRead && intRead < 6) {
-
-            current = intRead;
-            clearStrip();
-
-            draw(player, intRead, COLOR_RED);
-            drawGrid(COLOR_BLUE); //Draws whatever is in grid 
-            showStrip();
-        }
+    bool exitClap = 0;
+    int current = 9;
+    int player = 0; // 0 means O, 1 means X
+    int tieCounter = 0;
+    for(int i = 0; i < 9; i++){
+        grid[i] = 2;
     }
-
+    
+    
+    do {
+        if (Serial.available() > 0) {
+            // Read the most recent byte
+            int intRead = Serial.read() - 48;
+            
+            // swap edges due to its wiring
+            if (intRead == 0) 
+              intRead = 2;
+            else if (intRead == 2) 
+              intRead = 0;
+            else if (intRead == 3)
+                intRead = 5;
+            else if (intRead == 5)
+                intRead = 3;
+            else if (intRead == 6)
+                intRead = 8;
+            else if (intRead == 8)
+                intRead = 6;
+    
+            // limit intRead < 9 because we have 9 LEDs
+            if (current != intRead && intRead < 9) {
+    
+                current = intRead;
+                clearStrip();
+    
+                draw(player, intRead, COLOR_RED);
+                drawGrid(COLOR_BLUE); //Draws whatever is in grid 
+                showStrip();
+            }
+        }
+    
         if (current != 9 && doubleClap() && grid[current] == 2) { //returns 1 or 0
         	grid[current] = player; //saves player choice in grid array
         	clearStrip();
@@ -102,44 +83,73 @@ void loop ()
         	showStrip();
                 
             player = 1 - player;
-        }
-
-        if(isWinner()) {
-//                Serial.println(player, " Win");
-              delay(2000);
-              clearStrip();
-              fillStrip(COLOR_OFF);
-
-              int winner = 1-player;
-
-              draw(winner, 1, COLOR_GREEN);
-              drawW(winner, 3, COLOR_GREEN);
-              drawO(winner, 4, COLOR_GREEN);
-              drawN(winner, 3, COLOR_GREEN);
-//              drawCircle(0, COLOR_GREEN);
-              showStrip();
-              while(1) {
-                //maintain this state, until reset is hit
-              }
+            tieCounter = tieCounter + 1;
         }
         
+        if(isWinner()) {
+            delay(2000);
+            clearStrip();
+            fillStrip(COLOR_OFF);
+    
+            int winner = 1-player;
+            
+            draw(winner, 0, COLOR_GREEN);
+            draw(winner, 1, COLOR_GREEN);
+            draw(winner, 2, COLOR_GREEN);
+            drawW(64*3, COLOR_GREEN);
+            drawO(64*4, COLOR_GREEN);
+            drawN(64*5, COLOR_GREEN);
+            draw(winner, 6, COLOR_GREEN);
+            draw(winner, 7, COLOR_GREEN);
+            draw(winner, 8, COLOR_GREEN);
+          
+            showStrip();
+            while(exitClap == 0) {
+                exitClap = doubleClap();
+            }
+        }
+        
+        if(tieCounter == 9 && isWinner() == false){
+            delay(2000);
+            clearStrip;
+            fillStrip(COLOR_OFF);
+            
+            drawT(64*3, COLOR_GREEN);
+            drawI(64*4, COLOR_GREEN);
+            drawE(64*5, COLOR_GREEN);
+            
+            showStrip();
+            while(exitClap == 0) {
+                exitClap = doubleClap();
+            }
+        }
+    } while(exitClap == 0);
 }
 
 bool isWinner() {
-//        if (grid[0] != 2 && grid[0] == grid[1] && grid[1] == grid[2]) {
-//            return true;
-//        }
-//        else if (grid[3] != 2 && grid[3] == grid[4] && grid[4] == grid[5]) {
-//            return true;
-//        }
-    // because the daisy chain is zig zagged the verticals are as so
-    if (grid[0] != 2 && grid[0] == grid[5]) {
+
+    if (grid[0] != 2 && grid[0] == grid[1] && grid[0] == grid[2] && grid [1] == grid[2]) { // horizontals
       return true;
     }
-    else if (grid[1] != 2 && grid[1] == grid[4]) {
+    else if (grid[3] != 2 && grid[3] == grid[4] && grid[3] == grid[5] && grid[4] == grid[5]) {
       return true;
     }
-    else if (grid[2] != 2 && grid[2] == grid[3]) {
+    else if (grid[6] != 2 && grid[6] == grid[7] && grid[6] == grid[8] && grid[7] == grid[8]) {
+      return true;
+    }
+    else if (grid[2] != 2 && grid[2] == grid[5] && grid[2] == grid[8] && grid[5] == grid[8]) { // verticals
+      return true;
+    }
+    else if (grid[1] != 2 && grid[1] == grid[4] && grid[1] == grid[7] && grid[4] == grid[7]) {
+      return true;
+    }
+    else if (grid[0] != 2 && grid[0] == grid[3] && grid[0] == grid[6] && grid[3] == grid[6]) {
+      return true;
+    }
+    else if (grid[2] != 2 && grid[2] == grid[4] && grid[2] == grid[6] && grid[4] == grid[6]) { // diagonals
+      return true;
+    }
+    else if (grid[0] != 2 && grid[0] == grid[4] && grid[0] == grid[8] && grid[4] == grid[8]) {
       return true;
     }
     return false;
@@ -162,7 +172,6 @@ void draw(int player, int position, ColorMap color) {
     else if (player == 1) {
         drawCross(position*64, color);
     }
-
 }
 
 
@@ -199,7 +208,11 @@ void drawCircle(int k, ColorMap color) {
 void drawW(int k, ColorMap color) {
   for(int i=0; i<8; i++) {
     for(int j=0; j<8; j++) {
-      if (i == 7 || j == 0 || j == 3 || j == 4 || j == 7) {
+      if ((i == 0 || i == 7) || 
+         (i == j && i < 4) || 
+         (i == 4 && j == 3) ||
+         (i == 5 && j == 2) ||
+         (i == 6 && j == 1)){
         setLEDColor(i*8+j+k, color);
       }
     }
@@ -215,7 +228,37 @@ void drawO(int k, ColorMap color) {
 void drawN(int k, ColorMap color) {
   for(int i=0; i<8; i++) {
     for(int j=0; j<8; j++) {
-      if (j == 0 || j == 7 || i == j) {
+      if (i == 0 || i == 7 || (i == 7-j)){
+        setLEDColor(i*8+j+k, color);
+      }
+    }
+  }
+}
+
+void drawT(int k, ColorMap color) {
+  for(int i=0; i<8; i++) {
+    for(int j=0; j<8; j++) {
+      if (i == 3 || i == 4 || j == 7 || j == 6){
+        setLEDColor(i*8+j+k, color);
+      }
+    }
+  }
+}
+
+void drawI(int k, ColorMap color) {
+  for(int i=0; i<8; i++) {
+    for(int j=0; j<8; j++) {
+      if (i == 6 || i == 7 || j == 4 || j == 3 || i == 1 || i == 0){
+        setLEDColor(i*8+j+k, color);
+      }
+    }
+  }
+}
+
+void drawE(int k, ColorMap color) {
+  for(int i=0; i<8; i++) {
+    for(int j=0; j<8; j++) {
+      if (i == 0 || i == 1 || (j != 5 && j != 2)){
         setLEDColor(i*8+j+k, color);
       }
     }
@@ -224,14 +267,9 @@ void drawN(int k, ColorMap color) {
 
 
 bool doubleClap() {
-  // put your main code here, to run repeatedly: 
   eventstatus = 0;  
   sensorValue = digitalRead(soundPin);  
   delay(1);
-  
-//   if(sensorValue == 1)
-//     Serial.println("yeah");
-  
   
   if(eventstatus == 0){
     if(sensorValue == 1){
@@ -262,10 +300,8 @@ bool doubleClap() {
   }
   
   if(eventstatus == 3){
-    
-    // Serial.println("activated");
-//    delay(500);
     return true;
   }
+  
   return false;
 }
